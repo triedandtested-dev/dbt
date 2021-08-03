9c11964fed69594914a88d94271732f36a6c04a0
Kyle Wigley <kyle@fishtownanalytics.com>
2021-08-03 15:02:56 -0400

    clean up todo

--- test/integration/base.py
+++ test/integration/base.py
@@ -141,7 +141,6 @@ class DBTIntegrationTest(unittest.TestCase):
 
     @property
     def database_host(self):
-        # TODO: rm
         return os.getenv('POSTGRES_TEST_HOST', 'localhost')
 
     def postgres_profile(self):
@@ -154,7 +153,7 @@ class DBTIntegrationTest(unittest.TestCase):
                     'default2': {
                         'type': 'postgres',
                         'threads': 4,
-                        'host': os.getenv('POSTGRES_TEST_HOST', 'localhost'),
+                        'host': self.database_host,
                         'port': int(os.getenv('POSTGRES_TEST_PORT', 5432)),
                         'user': os.getenv('POSTGRES_TEST_USER', 'root'),
                         'pass': os.getenv('POSTGRES_TEST_PASS', 'password'),
@@ -164,7 +163,7 @@ class DBTIntegrationTest(unittest.TestCase):
                     'noaccess': {
                         'type': 'postgres',
                         'threads': 4,
-                        'host': os.getenv('POSTGRES_TEST_HOST', 'localhost'),
+                        'host': self.database_host,
                         'port': int(os.getenv('POSTGRES_TEST_PORT', 5432)),
                         'user': 'noaccess',
                         'pass': 'password',
